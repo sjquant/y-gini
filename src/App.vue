78503b288f584db8a14f9591a827939965d34e7b
sjquant <seonujang92@gmail.com>
2020-08-19 14:19:27 +0900

    Add righ-click menu

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,49 @@
   </div>
 </template>
 <script>
+import { remote } from "electron";
+const { Menu } = remote;
+
+const createDefaultMenu = () => {
+  const template = [
+    {
+      label: "Undo",
+      role: "undo",
+    },
+    {
+      label: "Redo",
+      role: "redo",
+    },
+    {
+      type: "separator",
+    },
+    {
+      label: "Cut",
+      role: "cut",
+    },
+    {
+      label: "Copy",
+      role: "copy",
+    },
+    {
+      label: "Paste",
+      role: "paste",
+    },
+    {
+      label: "Select All",
+      role: "selectall",
+    },
+  ];
+  return Menu.buildFromTemplate(template);
+};
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  created() {
+    const menu = createDefaultMenu();
+    window.addEventListener("contextmenu", () => {
+      menu.popup(remote.getCurrentWindow());
+    });
+  },
+};
 </script>
